903225e25b83cb2a826ba4f73a6043368c9619ba
unknown <RmanHeydari2000@gmail.com>
2023-11-29 00:58:47 -0600

    four russian trick section completed and compare with the dynamic programming of O(n)

--- four_russian_trick.py
+++ four_russian_trick.py
@@ -1,2 +1,98 @@
-def four_russian_trick(text, pattern):
-    return True
+import math
+
+
+# Function to count the number of '1's in a binary string
+def ones(binary_string):
+    return binary_string.count('1')
+
+
+# Function to calculate the maximum value of k
+def calculate_k_max(F, G):
+    # If F is longer than G, pad G with '0's
+    if len(F) > len(G):
+        G += (len(F) - len(G)) * '0'
+    max_k = 0
+    k = 0
+    # Iterate over the length of F
+    for i in range(len(F)):
+        # If the bits are the same, k remains the same
+        if F[i] == G[i]:
+            k += 0
+        # If F has a '1' and G has a '0', decrement k
+        elif F[i] == '1' and G[i] == '0':
+            k -= 1
+        # If F has a '0' and G has a '1', increment k
+        elif F[i] == '0' and G[i] == '1':
+            k += 1
+        # Update max_k if k is greater
+        max_k = max(max_k, k)
+    return max_k
+
+
+# Function to calculate the minimum value of k
+def calculate_k_min(F, G):
+    # If F is longer than G, pad G with '1's
+    if len(F) > len(G):
+        G += (len(F) - len(G)) * '1'
+    min_k = 0
+    k = 0
+    # Iterate over the length of F
+    for i in range(len(F)):
+        # If the bits are the same, k remains the same
+        if F[i] == G[i]:
+            k += 0
+        # If F has a '1' and G has a '0', decrement k
+        elif F[i] == '1' and G[i] == '0':
+            k -= 1
+        # If F has a '0' and G has a '1', increment k
+        elif F[i] == '0' and G[i] == '1':
+            k += 1
+        # Update min_k if k is smaller
+        min_k = min(min_k, k)
+    return min_k
+
+
+# Function to implement the Four Russians trick
+def four_russian_trick(text, l):
+    # Initialize values
+    n = len(text)
+    s = int(math.log2(n))
+    A_max, A_min = {}, {}
+    B = 0
+    max_one_result, min_one_result = 0, n
+    # If s is 0, return the count of '1's in text
+    if s == 0:
+        return ones(text), ones(text)
+    # Iterate over the text in chunks of size s
+    for i in range(0, n, s):
+        w = text[i:i + l]
+        F = w[:s]
+        G = text[i + l: i + l + s]
+        B = ones(w)
+        # If (F, G) is not in A, calculate k_max and k_min and assign in A
+        if (F, G) not in A_max:
+            A_max[F, G], A_min[F, G] = calculate_k_max(F, G), calculate_k_min(F, G)
+        # Update results
+        max_one_result = max(max_one_result, B + A_max[F, G])
+        min_one_result = min(min_one_result, B + A_min[F, G])
+    return max_one_result, min_one_result
+
+
+# The dynamic programming approach, implemented just to compare
+def max_one_dp(text, l):
+    n = len(text)
+    s = int(math.log2(n) / 6)
+    w = text[:l]
+    w_ones = ones(w)
+    result = w_ones
+    for i in range(1, n - l + 1):
+        temp = 0
+        w = text[i:i + l]
+        if text[i - 1] == '0' and text[i + l - 1] == '1':
+            temp = 1
+        elif text[i - 1] == '1' and text[i + l - 1] == '0':
+            temp = -1
+        w_ones = w_ones + temp
+        result = max(result, w_ones)
+
+    return result
--- main.py
+++ main.py
@@ -1,44 +1,64 @@
-from dataset import generate_dataset, write_labels, load_dataset
+from dataset import generate_dataset, write_labels, load_dataset, generate_binary_string
 from sub_quadratic import sub_quadratic
 from table_lookup import table_lookup
-from four_russian_trick import four_russian_trick
+from four_russian_trick import four_russian_trick, max_one_dp
 import time
 
 if __name__ == '__main__':
-    generate_dataset(100)
-    data = load_dataset()[1:]
-    sub_quadratic_results, table_lookup_results, four_russian_trick_results = [], [], []
-    sub_quadratic_time, table_lookup_time, four_russian_trick_time = 0, 0, 0
-    flag = True
-    # Iterate samples and call each of the algorithms for each sample
-    for i, row in enumerate(data):
+    # generate_dataset(100, 1000)
+    # data = load_dataset()[1:]
+    # sub_quadratic_results, table_lookup_results, four_russian_trick_results = [], [], []
+    # sub_quadratic_time, table_lookup_time, four_russian_trick_time = 0, 0, 0
+    # flag = True
+    # # Iterate samples and call each of the algorithms for each sample
+    # for i, row in enumerate(data):
+    #     t = time.time()
+    #     # Save the result for each algorithm to compare
+    #     sub_quadratic_results.append(sub_quadratic(row[0], row[1]))
+    #     # Save the running time for each algorithm to compare
+    #     sub_quadratic_time += (time.time() - t)
+    #
+    #     t = time.time()
+    #     table_lookup_results.append(table_lookup(row[0], row[1]))
+    #     table_lookup_time += (time.time() - t)
+    #
+    #     t = time.time()
+    #     four_russian_trick_results.append(four_russian_trick(row[0], row[1]))
+    #     four_russian_trick_time += (time.time() - t)
+    #     # Check the compatibility of the results
+    #     if sub_quadratic_results[i] != table_lookup_results[i] or sub_quadratic_results[i] != \
+    #             four_russian_trick_results[i] or table_lookup_results[i] != four_russian_trick_results[i]:
+    #         print(f"Row {i} is different")
+    #         flag = False
+    #
+    # # Write the results in the csv files
+    # write_labels('table_lookup', table_lookup_results)
+    # write_labels('four_russian_trick', four_russian_trick_results)
+    # write_labels('sub_quadratic', sub_quadratic_results)
+    #
+    # if flag:
+    #     print("Results of the algorithms are same.")
+    #
+    # print("sub quadratic algorithm time:", sub_quadratic_time)
+    # print("table lookup algorithm time:", table_lookup_time)
+    # print("sub quadratic algorithm time:", four_russian_trick_time)
+    maxOne_time, maxOne_normal_time = 0, 0
+    for i in range(100):
+        random_binary = generate_binary_string(1000000)
         t = time.time()
-        # Save the result for each algorithm to compare
-        sub_quadratic_results.append(sub_quadratic(row[0], row[1]))
-        # Save the running time for each algorithm to compare
-        sub_quadratic_time += (time.time() - t)
+        res_1 = four_russian_trick(random_binary, 4)
+        maxOne_time += (time.time() - t)
 
         t = time.time()
-        table_lookup_results.append(table_lookup(row[0], row[1]))
-        table_lookup_time += (time.time() - t)
+        res_2 = max_one_dp(random_binary, 4)
+        maxOne_normal_time += (time.time() - t)
 
-        t = time.time()
-        four_russian_trick_results.append(four_russian_trick(row[0], row[1]))
-        four_russian_trick_time += (time.time() - t)
-        # Check the compatibility of the results
-        if sub_quadratic_results[i] != table_lookup_results[i] or sub_quadratic_results[i] != \
-                four_russian_trick_results[i] or table_lookup_results[i] != four_russian_trick_results[i]:
-            print(f"Row {i} is different")
-            flag = False
+        if res_1 != res_2:
+            print("shit!", res_1, res_2)
+            print(random_binary)
+            break
 
-    # Write the results in the csv files
-    write_labels('table_lookup', table_lookup_results)
-    write_labels('four_russian_trick', four_russian_trick_results)
-    write_labels('sub_quadratic', sub_quadratic_results)
-
-    if flag:
-        print("Results of the algorithms are same.")
-
-    print("sub quadratic algorithm time:", sub_quadratic_time)
-    print("table lookup algorithm time:", table_lookup_time)
-    print("sub quadratic algorithm time:", four_russian_trick_time)
+        if i % 10 == 0:
+            print('time for maxOne', maxOne_time)
+            print('time for maxOne_normal', maxOne_normal_time)
+            print('-------------')
